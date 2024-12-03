b31a6772121863c2c42959371915431d3e7ef261
verheyenemilie <emilie.verheyen@outlook.be>
2024-12-03 22:03:14 +0100

    config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config: 
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:password@localhost:port/dbname'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.rdxzfdalqpfctxpsttxb:Databasesystemen2024@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
