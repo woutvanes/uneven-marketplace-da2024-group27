b9e500f6d20ae1363eb7b58be59ee78f8b4a23f4
verheyenemilie <emilie.verheyen@outlook.be>
2024-12-03 22:03:29 +0100

    models.py

--- app/models.py
+++ app/models.py
@@ -1,21 +1,59 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
-
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    listings = db.relationship('Listing', backref='user', lazy=True)
+    __tablename__ = 'users'
+    userID = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(100), nullable=False)
+    authID = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
 
-    def __repr__(self):
-        return f'<User {self.username}>'
-    
-class Listing(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    listing_name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __repr__(self):
-        return f'<Listing {self.listing_name}, ${self.price}>'
+class Notification(db.Model):
+    __tablename__ = 'notifications'
+    notificationID = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50), nullable=False)
+    viewed = db.Column(db.Boolean, default=False, nullable=False)
+
+    # Foreign Key
+    receiverID = db.Column(db.Integer, db.ForeignKey('users.userID'), nullable=False)
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    listingID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    picture = db.Column(db.String(200), nullable=True)
+    status = db.Column(db.String(50), nullable=False)
+
+    # Foreign Key
+    providerID = db.Column(db.Integer, db.ForeignKey('users.userID'), nullable=False)
+
+    # Additional Field
+    available_calendar = db.Column(db.Text, nullable=False)
+
+
+class Booking(db.Model):
+    __tablename__ = 'bookings'
+    BookingID = db.Column(db.Integer, primary_key=True)
+    personsBooked = db.Column(db.Integer, nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    commissionfee = db.Column(db.Float, nullable=False)
+
+    # Foreign Keys
+    listingID = db.Column(db.Integer, db.ForeignKey('products.listingID'), nullable=False)
+    buyerID = db.Column(db.Integer, db.ForeignKey('users.userID'), nullable=False)
+
+    # Additional Field
+    booked_calendar = db.Column(db.Text, nullable=False)
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    reviewID = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer, nullable=False)
+
+    # Foreign Keys
+    buyerID = db.Column(db.Integer, db.ForeignKey('users.userID'), nullable=False)
+    BookingID = db.Column(db.Integer, db.ForeignKey('bookings.BookingID'), nullable=False)
